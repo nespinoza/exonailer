d387458c7dc49689919b738efe03af1b038991b7
nespinoza <nestor.espinozap@gmail.com>
2015-11-19 23:51:50 -0500

    Small changes to main codes.

--- exonailer.py
+++ exonailer.py
@@ -34,9 +34,17 @@ q2 = 0.5
 ################################################
 ############ DATA PRE-PROCESSING ###############
 # First, get the transit data:
-t,f = np.loadtxt('transit_data/'+target+'_lc.dat',unpack=True)
+try:
+    t,f,f_err = np.loadtxt('transit_data/'+target+'_lc.dat',unpack=True,usecols=(0,1,2))
+except:
+    t,f = np.loadtxt('transit_data/'+target+'_lc.dat',unpack=True,usecols=(0,1))
+    f_err = None
 # Now the RV data:
-t_rv,rv,rv_err = np.loadtxt('')
+try:
+    t_rv,rv,rv_err = np.loadtxt('rv_data/'+target+'_rvs.dat',unpack=True,usecols=(0,1,2))
+except:
+    t_rv,rv = np.loadtxt('rv_data/'+target+'_rvs.dat',unpack=True,usecols=(0,1)) 
+    rv_err = None
 
 # Now, the first phase in transit fitting is to 'detrend' the 
 # data. This is done with the 'detrend' flag. If 
@@ -48,6 +56,8 @@ if detrend is not None:
         from scipy.ndimage.filters import gaussian_filter
         filt = gaussian_filter(medfilt(f,window),5)
         f = f/filt
+        if f_err not None:
+            f_err = f_err/filt
 
 # Get the phases:
 phases = (t-t0)/P
@@ -76,14 +86,8 @@ theta_0 = P,inc,a,p,t0,q1,q2,sigma_w
 sigma_theta_0 = 0.1,5.,10.,0.1,0.1,2000.0
 
 # Run the MCMC:
-theta_out_wn = transit_utils.transit_mcmc_fit(t.astype('float64'), \
-                                             f.astype('float64'), None, theta_0, sigma_theta_0, \
-                                             ld_law,njumps=100, nburnin = 100, \
-                                             nwalkers = 100,  noise_model = 'white')
-
-P_c,inc_c,a_c,p_c,t0_c,q1_c,q2_c,sigma_w_c = transit_utils.transit_mcmc_fit(t.astype('float64'), \
-                                             f.astype('float64'), None, theta_0, sigma_theta_0, \
-                                             ld_law,njumps=100, nburnin = 100, \
+theta_out_wn = transit_utils.transit_mcmc_fit(t, f, f_err, theta_0, sigma_theta_0, \
+                                             ld_law, njumps=100, nburnin = 100, \
                                              nwalkers = 100,  noise_model = 'white')
 
 # Get plot of the white-noise fit:
--- fit_transit.py
+++ fit_transit.py
@@ -34,7 +34,11 @@ q2 = 0.5
 ################################################
 ############ DATA PRE-PROCESSING ###############
 # First, get the data:
-t,f = np.loadtxt('transit_data/'+target+'_lc.dat',unpack=True)
+try:
+    t,f,f_err = np.loadtxt('transit_data/'+target+'_lc.dat',unpack=True,usecols=(0,1,2))
+except:
+    t,f = np.loadtxt('transit_data/'+target+'_lc.dat',unpack=True,usecols=(0,1))
+    f_err = None
 
 # Now, the first phase in transit fitting is to 'detrend' the 
 # data. This is done with the 'detrend' flag. If 
@@ -46,6 +50,8 @@ if detrend is not None:
         from scipy.ndimage.filters import gaussian_filter
         filt = gaussian_filter(medfilt(f,window),5)
         f = f/filt
+        if f_err is not None:
+            f_err = f_err/filt
 
 # Get the phases:
 phases = (t-t0)/P
@@ -74,14 +80,8 @@ theta_0 = P,inc,a,p,t0,q1,q2,sigma_w
 sigma_theta_0 = 0.1,5.,10.,0.1,0.1,2000.0
 
 # Run the MCMC:
-theta_out_wn = transit_utils.transit_mcmc_fit(t.astype('float64'), \
-                                             f.astype('float64'), None, theta_0, sigma_theta_0, \
-                                             ld_law,njumps=100, nburnin = 100, \
-                                             nwalkers = 100,  noise_model = 'white')
-
-P_c,inc_c,a_c,p_c,t0_c,q1_c,q2_c,sigma_w_c = transit_utils.transit_mcmc_fit(t.astype('float64'), \
-                                             f.astype('float64'), None, theta_0, sigma_theta_0, \
-                                             ld_law,njumps=100, nburnin = 100, \
+theta_out_wn = transit_utils.transit_mcmc_fit(t, f, f_err, theta_0, sigma_theta_0, \
+                                             ld_law, njumps=100, nburnin = 100, \
                                              nwalkers = 100,  noise_model = 'white')
 
 # Get plot of the white-noise fit:
