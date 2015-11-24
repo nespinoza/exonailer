738d60b04bec540afaf089648ebc1cf851642e25
nespinoza <nestor.espinozap@gmail.com>
2015-11-24 15:13:33 -0300

    Added quantile estimation to general_utils.py code to get cred. intervals.

--- utilities/general_utils.py
+++ utilities/general_utils.py
@@ -33,3 +33,37 @@ def read_data(target,mode):
             t_rv,rv = np.loadtxt('rv_data/'+target+'_rvs.dat',unpack=True,usecols=(0,1))
         
     return t,f,f_err,t_rv,rv,rv_err
+
+def get_quantiles(dist,alpha = 0.68, method = 'median'):
+    """
+    get_quantiles function
+
+    DESCRIPTION
+
+        This function returns, in the default case, the parameter median and the error% 
+        credibility around it. This assumes you give a non-ordered 
+        distribution of parameters.
+
+    OUTPUTS
+
+        Median of the parameter,upper credibility bound, lower credibility bound
+
+    """
+    ordered_dist = dist[np.argsort(dist)]
+    param = 0.0
+    # Define the number of samples from posterior
+    nsamples = len(dist)
+    nsamples_at_each_side = int(nsamples*(alpha/2.)+1)
+    if(method == 'median'):
+       med_idx = 0
+       if(nsamples%2 == 0.0): # Number of points is even
+          med_idx_up = int(nsamples/2.)
+          med_idx_down = med_idx_up-1
+          param = (ordered_dist[med_idx_up]+ordered_dist[med_idx_down])/2.
+          return param,ordered_dist[med_idx_up+nsamples_at_each_side],\
+                 ordered_dist[med_idx_down-nsamples_at_each_side]
+       else:
+          med_idx = int(nsamples/2.)
+          param = ordered_dist[med_idx]
+          return param,ordered_dist[med_idx+nsamples_at_each_side],\
+                 ordered_dist[med_idx-nsamples_at_each_side]
