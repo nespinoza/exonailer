20e97c4d24061a3f8da8ae310fe7bea66fe78b21
nespinoza <nestor.espinozap@gmail.com>
2015-11-29 18:05:26 -0800

    Modified main code to test speed with previous version.

--- exonailer.py
+++ exonailer.py
@@ -35,25 +35,26 @@ rv_jitter = False
 # First, get the transit and RV data:
 t,f,f_err,t_rv,rv,rv_err = general_utils.read_data(target,mode)
 
-# Now read the priors:
-priors = general_utils.read_priors(target)
+# Initialize the parameters:
+parameters = general_utils.read_priors(target)
 
 # Pre-process the transit data if available:
 if mode != 'rvs':
     t,phases,f, f_err = transit_utils.pre_process(t,f,f_err,phot_detrend,\
                                                   phot_get_outliers,n_ommit,\
-                                                  window,priors,ld_law)
+                                                  window,parameters,ld_law)
+
 # Run the MCMC:
-theta_out = transit_utils.exonailer_mcmc_fit(t, f, f_err, t_rv, rv, rv_err, \
-                                             priors, ld_law, mode, rv_jitter = rv_jitter, \
-                                             njumps=500, nburnin = 500, \
-                                             nwalkers = 100,  noise_model = phot_noise_model)
-
-for chain_var in theta_out:
-    print np.median(chain_var),'+-',np.sqrt(np.var(chain_var))
-
+import time
+tic = time.time()
+transit_utils.exonailer_mcmc_fit(t, f, f_err, t_rv, rv, rv_err, \
+                                 parameters, ld_law, mode, rv_jitter = rv_jitter, \
+                                 njumps=5000, nburnin = 5000, \
+                                 nwalkers = 100,  noise_model = phot_noise_model)
+toc = time.time()
+print 'Took',toc-tic,' seconds to do 5000 jumps, 5000 burnins with 100 walkers.'
 # Get plot of the transit-fit:
 if mode == 'transit':
-    transit_utils.plot_transit(t,f,theta_out,ld_law)
+    transit_utils.plot_transit(t,f,parameters,ld_law)
 elif mode == 'full':
-    transit_utils.plot_transit_and_rv(t,f,t_rv,rv,rv_err,theta_out,ld_law,rv_jitter)
+    transit_utils.plot_transit_and_rv(t,f,t_rv,rv,rv_err,parameters,ld_law,rv_jitter)
