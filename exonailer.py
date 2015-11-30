cf9d1f8cc203a8e5f5efcc9a90899adea2add702
nespinoza <nestor.espinozap@gmail.com>
2015-11-30 13:08:19 -1000

    Added emcee options.

--- exonailer.py
+++ exonailer.py
@@ -9,16 +9,16 @@ import numpy as np
 ################# OPTIONS ######################
 
 # Define the target name, detrending method and parameters of it:
-target = 'CL005-04'
+target = 'CL001-04'
 phot_noise_model = 'white'
 phot_detrend = 'mfilter'
-window = 21#41
+window = 41
 
 # Define if you want to perform automatic outlier removal (sigma-clipping):
 phot_get_outliers = True
 
 # Define which transits you want to ommit (counted from first transit):
-n_ommit = [3,9]
+n_ommit = []#[3,9]
 
 # Limb-darkening law to be used:
 ld_law = 'quadratic'
@@ -28,6 +28,11 @@ mode = 'full'
 
 # Define noise properties:
 rv_jitter = False
+
+# Define emcee parameters:
+nwalkers = 100
+njumps = 5000
+nburnin = 5000
 
 ################################################
 
@@ -53,13 +58,14 @@ if not os.path.exists('results'):
 if not os.path.exists('results/'+target+'_'+mode+'_'+phot_noise_model+'_'+ld_law):
     transit_utils.exonailer_mcmc_fit(t, f, f_err, t_rv, rv, rv_err, \
                                      parameters, ld_law, mode, rv_jitter = rv_jitter, \
-                                     njumps=1000, nburnin = 2000, \
-                                     nwalkers = 100,  noise_model = phot_noise_model)
+                                     njumps = njumps, nburnin = nburnin, \
+                                     nwalkers = nwalkers,  noise_model = phot_noise_model)
 
     general_utils.save_results(target,mode,phot_noise_model,ld_law,parameters)
 
 else:
     parameters = general_utils.read_results(target,mode,phot_noise_model,ld_law)
+
 # Get plot of the transit-fit:
 if mode == 'transit':
     transit_utils.plot_transit(t,f,parameters,ld_law)
